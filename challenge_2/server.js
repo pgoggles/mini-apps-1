ddf4226d93b27df45bfc01857138d14a7872a60d
pgoggles <praneeth.gogineni@gmail.com>
2021-04-06 10:18:35 -0700

    Initialize and Setup Express Routing (Preliminary)

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,14 @@
+const express = require('express');
+const app = express();
+const port = 3001;
+const path = require('path');
 
+app.listen(port, () => {
+  console.log(`JSON to CSV Listening on Port ${port}`);
+});
+
+app.get('/', (req, res) => {
+  console.log('Serving up get request');
+  res.sendFile(path.join(__dirname, 'client/index.html'));
+});
 
