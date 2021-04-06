ddf7be5f082a66a5a02502f4fb77b7bd7c20f68a
pgoggles <praneeth.gogineni@gmail.com>
2021-04-06 11:20:53 -0700

    Implement JSON to CSV Parser

--- challenge_2/server/parser.js
+++ challenge_2/server/parser.js
@@ -1,3 +1,37 @@
-module.exports.parser = function(JSON) {
-  return JSON.stringify(JSON);
+module.exports.parser = function(json) {
+  var parsedJSON = JSON.parse(json['form']);
+  var CSV = '';
+  var keys = Object.keys(parsedJSON);
+  keys.splice(keys.indexOf('children'), 1);
+  for (var i = 0; i < keys.length; i++) {
+    CSV += keys[i];
+    if (i !== keys.length - 1) {
+      CSV += ',';
+    } else {
+      CSV += '\n';
+    }
+  }
+  var recursiveAdd = function (jsonBody) {
+    var children = jsonBody['children'];
+    delete jsonBody['children'];
+    var keys = Object.keys(jsonBody);
+    var lastKey = keys[keys.length - 1];
+    for (var key in jsonBody) {
+      if (key !== 'children') {
+        CSV += jsonBody[key];
+      }
+      if (key !== lastKey) {
+        CSV += ',';
+      } else {
+        CSV += '\n';
+      }
+    }
+    if (children.length > 0) {
+      for (var i = 0; i < children.length; i++) {
+        recursiveAdd(children[i]);
+      }
+    }
+  };
+  recursiveAdd(parsedJSON);
+  return CSV;
 };
--- challenge_2/server/server.js
+++ challenge_2/server/server.js
@@ -3,6 +3,8 @@ const app = express();
 const port = 3001;
 const path = require('path');
 const bodyParser = require('body-parser');
+const JSONParser = require('./parser.js');
+
 app.use(bodyParser.urlencoded());
 app.use(bodyParser.json());
 
@@ -11,16 +13,16 @@ app.listen(port, () => {
 });
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client/index.html'));
+  res.sendFile(path.join(__dirname, '../client/index.html'));
 });
 
 app.get('/app.js', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client/app.js'));
+  res.sendFile(path.join(__dirname, '../client/app.js'));
 });
 
 app.post('/json', (req, res) => {
-  console.log(req.body);
+  var parsedCSV = JSONParser.parser(req.body);
   res.status(200);
-  res.end();
+  res.end(parsedCSV);
 });
 
