4fed22a6be2c31572ed643892c157b346ad4a385
pgoggles <praneeth.gogineni@gmail.com>
2021-04-07 14:46:14 -0700

    Reimplement Server to use a Static Path

--- challenge_3/server/index.js
+++ challenge_3/server/index.js
@@ -3,12 +3,10 @@ const express = require('express');
 const app = express ();
 const port = 3001;
 
+app.use(express.static('public'));
+
 app.get('/', (req, res) => {
   res.sendFile(path.resolve(__dirname, '..', 'public', 'index.html'));
-});
-
-app.get('/index.js', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '..', 'public', 'index.js'));
 });
 
 app.listen(port, () => {
