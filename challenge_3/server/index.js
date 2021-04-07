6487b3797ca88e268f231ce863ea965bc9294006
pgoggles <praneeth.gogineni@gmail.com>
2021-04-07 14:35:34 -0700

    Add Server Route for Bundle.js

--- challenge_3/server/index.js
+++ challenge_3/server/index.js
@@ -7,6 +7,10 @@ app.get('/', (req, res) => {
   res.sendFile(path.resolve(__dirname, '..', 'public', 'index.html'));
 });
 
+app.get('/bundle.js', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '..', 'client', 'dist', 'bundle.js'));
+});
+
 app.listen(port, () => {
   console.log(`Listening on ${port}`);
 });
