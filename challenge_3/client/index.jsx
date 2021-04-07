39c5df4b021964e78fe2dedf2ee009447355ecef
pgoggles <praneeth.gogineni@gmail.com>
2021-04-07 14:59:56 -0700

    Reimplement App to be in one file

--- challenge_3/client/index.jsx
+++ challenge_3/client/index.jsx
@@ -1,4 +1,21 @@
-import App from './app.js';
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {currentPage: 0};
+  }
+
+  render() {
+    if (this.state.currentPage === 0) {
+      return (
+        <div>THIS IS A TEST PAGE</div>
+      );
+    } else {
+      return (
+        <div>THIS IS A SECOND TEST PAGE</div>
+      );
+    }
+  }
+}
 
 ReactDOM.render(
   <App />,
