11e6f21f4c3093b6ff72da135c844c3a21bee022
pgoggles <praneeth.gogineni@gmail.com>
2021-04-07 14:32:33 -0700

    Implement Server Express Routes

--- challenge_3/server/index.js
+++ challenge_3/server/index.js
@@ -1,2 +1,12 @@
+const path = require ('path');
+const express = require('express');
+const app = express ();
+const port = 3001;
 
+app.get('/', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '..', 'public', 'index.html'));
+});
 
+app.listen(port, () => {
+  console.log(`Listening on ${port}`);
+});
