d7b224e9d5ef580b2c6dc035fe2aa08ded517219
pgoggles <praneeth.gogineni@gmail.com>
2021-04-08 09:50:17 -0700

    Initialize Server on Port 3001

--- challenge_4/server.js
+++ challenge_4/server.js
@@ -1,2 +1,11 @@
+const express = require('express');
+const app = express();
+const port = 3001;
 
+app.get('/', (req, res) => {
+  res.send('TEST');
+});
 
+app.listen(port, () => {
+  console.log(`Listening on ${port}`);
+});
