d134a5ae2f304b018a99b3cd2a422a40faf6306e
pgoggles <praneeth.gogineni@gmail.com>
2021-04-08 11:11:23 -0700

    Reimplement Grid Creation to be more efficient and compact

--- challenge_4/client/MakeGridEntry.jsx
+++
@@ -1,8 +0,0 @@
-var MakeGridEntry = function (props) {
-  var classString = 'emptyCircle Row' + props.rowNumber + 'Column' + props.columnNumber;
-  return (
-    <div class={classString}></div>
-  );
-};
-
-export default MakeGridEntry;
--- challenge_4/client/app.jsx
+++ challenge_4/client/app.jsx
@@ -1,12 +1,35 @@
-import MakeGrid from './MakeGrid.jsx';
 class App extends React.Component {
   constructor(props) {
     super(props);
+    state: {
+
+    }
+  }
+
+  gridInitialize (n) {
+    var gameBoard = [];
+    for (var i = 0; i < n; i++) {
+      var subBoard = [];
+      for (var j = 0; j < n; j++) {
+        subBoard.push({value: 0, indices: 'R' + i + 'C' + j});
+      }
+      gameBoard.push(subBoard);
+    }
+    return gameBoard;
   }
   render () {
+    var gameBoard = this.gridInitialize(7);
     return (
       <div>
-        <MakeGrid n={7} />
+        {gameBoard.map(row => (
+          <div>
+            {row.map(gridEntry => {
+              var classString = 'emptyCircle ' + gridEntry.indices;
+              return <div class={classString}></div>;
+            }
+            )}
+          </div>
+        ))}
       </div>
     );
   }
