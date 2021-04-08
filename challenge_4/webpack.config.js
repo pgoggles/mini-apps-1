e3235f78581f4ab92c04e7b40c41d59ebe807a24
pgoggles <praneeth.gogineni@gmail.com>
2021-04-08 09:47:19 -0700

    Initialize Webpack, Babel, and NPM files

---
+++ challenge_4/webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+
+module.exports = {
+  entry: './client/app.jsx',
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'app.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        }
+      }
+    ]
+  }
+};
